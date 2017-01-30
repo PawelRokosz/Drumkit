6bb9cb856086e7438d8598500a81d6e724360e15
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-01-31 00:40:56 +0100

    changed structure of soundPlay function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function soundPlay(e){
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
+   if(!audio) return;
+   audio.currentTime = 0;
+   audio.play();
 
-  key.classList.add('played');
-});
+   key.classList.add('played');
+}
 
 function removeTransition(e){
   if(e.propertyName !== 'transform') return;
@@ -16,3 +16,4 @@ function removeTransition(e){
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', soundPlay);
