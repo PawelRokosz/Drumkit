6767f44bf39b799f2e8095cdff8ef0e35d29d9ed
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-01-31 00:11:26 +0100

    class with styles added when key is pressed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return;
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+  if(!audio) return;
   audio.currentTime = 0;
   audio.play();
+
+  key.classList.add('played');
 });
